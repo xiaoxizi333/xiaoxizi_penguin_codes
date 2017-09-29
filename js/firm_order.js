7e469c6a93a1fdcbfc65b2be3d6679f0c9d57117
xiaoxizi <379044297@qq.com>
2017-09-30 00:24:39 +0800

    firm

--- js/firm_order.js
+++ js/firm_order.js
@@ -1,4 +1,5 @@
 if(!openid){
+	sessionStorage.setItem("redirect_url",window.location.href);
 	window.location.href="https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx795992462b631e70&redirect_uri=http%3A%2F%2Fshop.qietuan.org%2Foauth.php&response_type=code&scope=snsapi_userinfo&state=12345678901#wechat_redirect"
 }
 isVip();
@@ -679,5 +680,5 @@ function delivery(){
 	});
 }
 $('.mask_icon').on('tap',function(){
-	showTips('暂无此选项');
+	showTips('暂不支持此配送方式');
 })
