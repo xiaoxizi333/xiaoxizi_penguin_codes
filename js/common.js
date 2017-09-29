167ded654a21097b70300495672acfc555ebfef7
xiaoxizi <379044297@qq.com>
2017-09-30 00:15:22 +0800

    common 跳转

--- js/common.js
+++ js/common.js
@@ -141,6 +141,7 @@ function switchDate(time,mark){
 //跳转购物车
 $('.shopping_icon').off('tap').on('tap',function(){
 	if(!openid){
+		sessionStorage.setItem("redirect_url",window.location.href);
 		window.location.href="https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx795992462b631e70&redirect_uri=http%3A%2F%2Fshop.qietuan.org%2Foauth.php&response_type=code&scope=snsapi_userinfo&state=12345678901#wechat_redirect"
 	}else{
 		if(uid){
@@ -165,6 +166,7 @@ $('.shopping_icon').off('tap').on('tap',function(){
 })
 $('.personal_icon').on('tap',function(){
 	if(!openid){
+		sessionStorage.setItem("redirect_url",window.location.href);
 		window.location.href="https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx795992462b631e70&redirect_uri=http%3A%2F%2Fshop.qietuan.org%2Foauth.php&response_type=code&scope=snsapi_userinfo&state=12345678901#wechat_redirect"
 	}else{
 		if(uid){
