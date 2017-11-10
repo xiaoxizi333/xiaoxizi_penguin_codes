c2b4c0fd1a05e16b02baab5e846d5b22d3aa24a5
xiaoxizi <379044297@qq.com>
2017-11-10 20:23:52 +0800

    广告

--- js/index.js
+++ js/index.js
@@ -2,6 +2,13 @@ if(!openid){
 	localStorage.setItem("redirect_url",window.location.href);
 	window.location.href="https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx795992462b631e70&redirect_uri=http%3A%2F%2Fshop.qietuan.org%2Foauth.php&response_type=code&scope=snsapi_userinfo&state=12345678901#wechat_redirect"
 }
+setTimeout(function(){
+	$('.adver_mask').fadeIn();
+},200)
+
+$('.adver_delete img').on('tap',function(){
+	$('.adver_mask').fadeOut();
+})
 //分类详情
 $.post(config.classify,{'is_add_best_ares':0,'limit':6},function(data){
 	//console.log(data);
