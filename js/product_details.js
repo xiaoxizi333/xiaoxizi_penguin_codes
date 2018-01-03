bc20356061d1ef7f8d0a35e12b9ad828116c8204
xiaoxizi <379044297@qq.com>
2018-01-03 13:09:41 +0800

    30优惠券

--- js/product_details.js
+++ js/product_details.js
@@ -1,5 +1,5 @@
+localStorage.setItem("redirect_url",window.location.href);
 if(!openid){
-	localStorage.setItem("redirect_url",window.location.href);
 	window.location.href="https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx795992462b631e70&redirect_uri=http%3A%2F%2Fshop.qietuan.org%2Foauth.php&response_type=code&scope=snsapi_userinfo&state=12345678901#wechat_redirect"
 }
 window.localStorage.setItem('isSelectPoint','0');
